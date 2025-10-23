348ed5ca804400b22123681d4fb273c93f77a0c7
Khushi Srivastava <sweetkhushi2000@gmail.com>
2025-10-24 00:18:37 +0530

    learned about data types and basic js

---
+++ 11. JS Datatypes/app.js
@@ -0,0 +1,32 @@
+// console.log("hello world!");
+// console.error("this is an error");
+// console.warn("this is a warning");
+
+// console.log(alert("hiiii"));
+
+// let apple = "fruit";
+// console.log(apple);
+
+// const banana = "sweet";
+// banana = "sour";
+// console.log(banana);
+
+// let variable = "";
+// let variable = "ghytesz";
+// let variable = "gbgtyiu jkjgv";
+// let variable = "4678";
+// let variable = string;
+// let variable = 98;
+// let variable = ['true', 'false', true, 765, 'named hydrogen'];
+// let variable = -98;
+// let variable = 67.545;
+// let variable = false;
+
+// let variable = {
+//   name: "ramesh",
+//   address: "gorakhpur",
+//   pinCode: 876543,
+//   age: 45,
+// };
+
+// console.log(typeof variable);
